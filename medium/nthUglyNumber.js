9d0f4ce14b7fb0f3e2fe55dddf24f700a0e728e1
EricRuxton <eruxton@outlook.com>
2025-01-14 21:05:54 -0400

    nthUglyNumber

---
+++ medium/nthUglyNumber.js
@@ -0,0 +1,40 @@
+var getNextUgly = function(lastUgly, twos, threes, fives) {
+    if (twos[0] <= threes[0] && twos[0] <= fives[0]){
+        if (twos[0] > lastUgly){
+            return twos.shift()
+        } 
+        twos.shift()
+    }
+    if (threes[0] <= twos[0] && threes[0] <= fives[0]){
+        if (threes[0] > lastUgly){
+            return threes.shift()
+        } 
+        threes.shift()
+    }
+    if (fives[0] <= twos[0] && fives[0] <= threes[0]){
+        if (fives[0] > lastUgly){
+            return fives.shift()
+        } 
+        fives.shift()
+    }
+    return getNextUgly(lastUgly, twos, threes, fives)
+}
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function(n) {
+    let uglies = [1]
+    let twos = []
+    let threes = []
+    let fives = []
+    while (uglies.length < n) {
+        twos.push(lastUgly*2); threes.push(lastUgly*3); fives.push(lastUgly*5)
+        uglies.push(getNextUgly(uglies[uglies.length - 1], twos, threes, fives))
+    }
+    console.log(uglies[uglies.length - 1])
+    return uglies[uglies.length - 1]
+};
+
+nthUglyNumber(10)
