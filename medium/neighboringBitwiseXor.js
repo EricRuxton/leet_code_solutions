6cf600744f7ba5fa52e7a84f6619037306487179
EricRuxton <eruxton@outlook.com>
2025-01-17 14:42:01 -0400

    doesValidArrayExist (bitwise)

---
+++ medium/neighboringBitwiseXor.js
@@ -0,0 +1,18 @@
+const ODD = []
+const EVEN = [1,1]
+
+/**
+ * @param {number[]} derived
+ * @return {boolean}
+ */
+var doesValidArrayExist = function(derived) {
+    let out = derived[0] == 1 ? false : true
+    for (let i = 1; i < derived.length; ++i){
+        if (derived[i] == 1){
+            out = !out
+        }
+    }
+    return out
+};
+
+doesValidArrayExist([1,1,0])
